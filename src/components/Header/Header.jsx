35348fc584662269ca9a0aa34cc5af2d4b03752f
Shoaib Saleem <muhammadshoaib0299@gmail.com>
2024-09-01 17:14:54 +0500

    header footer

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <div className='text-center text-4xl font-bold'>Header</div>
+  )
+}
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <div className='flex items-center justify-center h-52 text-4xl font-bold'>Home</div>
+  )
+}
