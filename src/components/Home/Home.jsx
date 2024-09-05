4e71c5a0a331888013dca1225bbe8af8d6e2e7db
shoaibsaleem0299 <shoaib0299@gmail.com>
2024-09-05 19:23:51 +0500

    make hero is now complete

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,39 +2,19 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <section >
-      <div className="bg-[url('/assets/images/bg-1.png')] w-[95%] h-screen mx-auto px-10 pt-32 rounded-[70px]">
-        {/* Row 1 */}
-        <div className="flex">
-          {/* Column 1 */}
-          <div className=" text-white">
-            <h1 className="text-5xl font-semibold">
-              Get <span className="text-blue-500">Best Products</span> With Lowest Price.
-            </h1>
-            <p className="mt-6 text-lg font-light">
-            Empower your shopping experience with seamless browsing, exclusive deals, and fast, secure checkout <br />
-            <span className="text-blue-500">welcome to the future of online shopping!</span>
-            </p>
-
-            {/* Buttons Row */}
-            <div className="flex flex-wrap mt-8">
-              <div className="w-full lg:w-5/12 mb-4">
-                <button className="bg-blue-500 text-white font-bold py-3 px-6 rounded-full hover:bg-white hover:text-blue-900 transition-all">
-                  Explore Now
-                  <i className="fas fa-arrow-right-from-bracket ml-3 bg-blue-700 text-white p-3 rounded-full hover:bg-white hover:text-blue-900 transition-all"></i>
-                </button>
-              </div>
-              <div className="w-full lg:w-2/12 mb-4 flex items-center justify-center lg:justify-start">
-                <i className="fas fa-play bg-white text-blue-900 p-5 rounded-full hover:bg-blue-700 hover:text-white transition-all"></i>
-              </div>
-              <div className="w-full lg:w-5/12 mb-4 text-center lg:text-left">
-                <a href="#" className="text-blue-700 text-sm hover:text-white transition-all">
-                  Watch Promotions
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
+    <section className='flex items-center justify-center' >
+      <div className="bg-[url('/assets/images/bg-1.png')] text-center w-[95%]  mx-auto px-10 py-32 rounded-[70px] ">
+        <h1 className="lg:text-6xl sm:text-4xl font-extrabold text-white mb-4">
+          Get <span className="text-blue-500">Best Products</span> With Lowest Price.
+        </h1>
+        <p className="text-white lg:text-xl md:text-sm font-semibold my-6">
+          Empower your shopping experience with seamless browsing, exclusive deals, and fast, secure checkout <br />
+          <span className="text-blue-500">welcome to the future of online shopping!</span>
+        </p>
+        <button className="bg-blue-500 text-white lg:font-bold md:font-light py-3 px-6 rounded-full hover:bg-white hover:text-blue-900 transition-all">
+          Explore Now
+          <i className="fas fa-arrow-right-from-bracket ml-3 bg-blue-700 text-white p-3 rounded-full hover:bg-white hover:text-blue-900 transition-all"></i>
+        </button>
       </div>
     </section>
   );
