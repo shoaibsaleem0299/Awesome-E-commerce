729b54650d6b3942671f5a8da1fb7b48bb4ffd2b
Shoaib Saleem <muhammadshoaib0299@gmail.com>
2024-09-12 23:24:36 +0500

    making product card component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -20,6 +20,10 @@ const Home = () => {
           imageUrl="https://content.jdmagicbox.com/quickquotes/images_main/bisleri-fonzo-600-ml-359983121-mn99t.png?impolicy=queryparam&im=Resize=(360,360),aspect=fit"
           title="Cold Drinks" />
       </div>
+      <div className='w-[90%] flex flex-wrap justify-evenly items-center m-auto'>
+        <img className='w-[40%] h-[85vh]  rounded-3xl ' src="/assets/images/bg-3.jpeg" alt="bg-3"/>
+        <img className='w-[40%] h-[85vh] rounded-3xl ' src="/assets/images/bg-2.png" alt="bg-3"/>
+      </div>
     </>
   );
 };
--- src/components/Home/components/CategorySection.jsx
+++ src/components/Home/components/CategorySection.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 export function CategorySection(props) {
     console.log(props);
     return (
-        <div className="flex flex-col items-center w-1/4">
-            <img className="bg-gray-200 rounded-full w-1/2 h-44 px-8 py-6 shadow-md" src={props.imageUrl} />
+        <div className="flex flex-col items-center ">
+            <img className="bg-gray-200 rounded-full h-44 px-8 py-6 shadow-md" src={props.imageUrl} />
             <Link to="/">
                 <button className="relative bottom-8  bg-white py-4 px-10 text-sm font-extrabold rounded-full shadow-lg hover:text-white hover:bg-blue-500 hover:duration-300">{props.title}</button>
             </Link>
